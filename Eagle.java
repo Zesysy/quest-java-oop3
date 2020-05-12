6890379bcef4a5aac7dd5ea55ca065bb9bbac0dd
Sylène <sylene.manusset@gmail.com>
2020-05-12 18:18:28 +0200

    fix quest

--- Eagle.java
+++ Eagle.java
@@ -1,4 +1,4 @@
-public class Eagle extends Bird {
+public class Eagle extends Bird implements Fly {
 
     private boolean flying;
     private int altitude;
@@ -21,4 +21,66 @@ public class Eagle extends Bird {
     public String sing() {
         return "Screech!";
     }
+
+    /**
+     * takes off, must be on ground
+     */
+    @Override
+    public void takeOff() {
+        if (!this.flying && this.altitude == 0) {
+            this.flying = true;
+            System.out.printf("%s takes off in the sky.%n", this.getName());
+        }
+    }
+
+    /**
+     * fly downward
+     * @param meters altitudes increase
+     * @return altitude
+     */
+    @Override
+    public int descend(int meters) {
+        if (this.flying) {
+            this.altitude = Math.min(this.altitude + meters, 325);
+            System.out.printf("%s flies downward, altitude : %d%n", this.getName() , this.altitude);
+        }
+        return this.altitude;
+    }
+
+    /**
+     * glides, must be in the sky
+     */
+    @Override
+    public void glide() {
+        if (this.flying && this.altitude > 0) {
+            System.out.println("It glides into the air.");
+        }
+    }
+
+    /**
+     * fly upward
+     * @param meters altitudes decrease
+     * @return altitude
+     */
+    @Override
+    public int ascend(int meters) {
+        if (this.flying) {
+            this.altitude = Math.max(this.altitude - meters, 0);
+            System.out.printf("%s flies upward, altitude : %d%n", this.getName(), this.altitude);
+        }
+        return this.altitude;
+    }
+
+    /**
+     * landed on the ground, must be on the air
+     */
+    @Override
+    public void land() {
+        if (this.flying && this.altitude == 0) {
+            System.out.printf("%s lands on the ground.%n", this.getName());
+        } else {
+            System.out.printf("%s is too high, it can't lands.%n", this.getName());
+        }
+    }
+
 }
---
+++ Fly.java
@@ -0,0 +1,13 @@
+public interface Fly {
+
+    void takeOff();
+
+    int ascend(int meters);
+
+    void glide();
+
+    int descend(int meters);
+
+    void land();
+
+}
